8a41262ccbfacb14fbf3c7a1466c11e34ce734bf
Carlos Aguilera <18aguilerac@gmail.com>
2025-01-28 16:33:34 -0800

    dims

--- client/src/assets/utility/dimensions.js
+++
@@ -1,9 +0,0 @@
-export function isMobile() {
-  return /iPhone|iPad|iPod|Android|webOS|BlackBerry|Windows Phone/i.test(
-    navigator.userAgent,
-  );
-}
-
-export function isMobileDimensions() {
-  return window.innerWidth <= 400 && window.innerHeight <= 800;
-}
