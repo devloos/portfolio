b7e7339054cda61b3eff13ed492e09c535100002
Carlos Aguilera <18aguilerac@gmail.com>
2025-04-18 12:58:25 -0700

    migrate to supabase

--- server/src/Entity/BaseEntity.php
+++ server/src/Entity/BaseEntity.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use App\Trait\ToArrayTrait;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\MappedSuperclass;
 
@@ -13,29 +14,29 @@ class BaseEntity
     use ToArrayTrait;
 
     #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
-    protected ?\DateTimeImmutable $createdAt = null;
+    protected ?DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
-    protected ?\DateTimeImmutable $modifiedAt = null;
+    protected ?DateTimeImmutable $modifiedAt = null;
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getCreatedAt(): ?DateTimeImmutable
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    public function setCreatedAt(DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
 
         return $this;
     }
 
-    public function getModifiedAt(): ?\DateTimeImmutable
+    public function getModifiedAt(): ?DateTimeImmutable
     {
         return $this->modifiedAt;
     }
 
-    public function setModifiedAt(\DateTimeImmutable $modifiedAt): static
+    public function setModifiedAt(DateTimeImmutable $modifiedAt): static
     {
         $this->modifiedAt = $modifiedAt;
 
