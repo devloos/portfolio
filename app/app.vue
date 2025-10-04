b4771901b6bc6b0358b174a52abbc7f9177e7f02
Carlos Aguilera <18aguilerac@gmail.com>
2025-10-04 15:16:28 -0700

    feat: implement NeuralBg component with dynamic background effects and integrate it into app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NeuralBg from './components/ui/bg-neural/NeuralBg.vue';
+
+const BG_DARK_HEX = '#1B1717';
+const BG_LIGHT_HEX = '#F1F1F1';
+
+const colorMode = useColorMode();
+
+const bgColor = computed(() => {
+  if (colorMode.preference === 'dark') {
+    return BG_DARK_HEX;
+  }
+
+  return BG_LIGHT_HEX;
+});
+</script>
 
 <template>
-  <div>hello</div>
+  <div class="relative">
+    <Teleport to="body">
+      <NeuralBg :backgroundColor="bgColor" :hue="0" :saturation="0" :chroma="0" />
+    </Teleport>
+
+    <Theme />
+  </div>
 </template>
