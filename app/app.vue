3871070db3e0d01a065dc90315b2c842262c9bea
Carlos Aguilera <18aguilerac@gmail.com>
2025-10-04 15:43:12 -0700

    feat: refactor NeuralBg component and integrate it into app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import NeuralBg from './components/ui/bg-neural/NeuralBg.vue';
-
 const BG_DARK_HEX = '#1B1717';
 const BG_LIGHT_HEX = '#F1F1F1';
 
@@ -26,7 +24,13 @@ const chroma = computed(() => {
 <template>
   <div class="relative">
     <Teleport to="body">
-      <NeuralBg :backgroundColor="bgColor" :hue="0" :saturation="0" :chroma="chroma" />
+      <NeuralBg
+        class="-z-10"
+        :backgroundColor="bgColor"
+        :hue="0"
+        :saturation="0"
+        :chroma="chroma"
+      />
     </Teleport>
 
     <Theme />
