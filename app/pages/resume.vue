fe1fcf082c134c3389a2d277625decbe3cf3b8d4
Carlos Aguilera <18aguilerac@gmail.com>
2025-10-05 23:57:43 -0700

    Implement code changes to enhance functionality and improve performance

--- app/pages/resume.vue
+++ app/pages/resume.vue
@@ -2,7 +2,7 @@
   <div class="w-full">
     <ClientOnly>
       <embed
-        src="/assets/resume.pdf"
+        src="/assets/carlos-aguilera-resume.pdf"
         class="aspect-[8.5/11]"
         type="application/pdf"
         width="100%"
